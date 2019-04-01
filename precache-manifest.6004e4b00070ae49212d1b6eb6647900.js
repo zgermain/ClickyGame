8d1052fec288af591b31f7ede0c8b79d12546051
ZGermain <zack.germain@gmail.com>
2019-04-01 07:30:53 -0500

    Updates

---
+++ precache-manifest.6004e4b00070ae49212d1b6eb6647900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.038e769c.js"
+  },
+  {
+    "revision": "1cb78c3c1b959033b94b",
+    "url": "/ClickyGame/static/js/main.1936349e.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/ClickyGame/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "1cb78c3c1b959033b94b",
+    "url": "/ClickyGame/static/css/main.d126daf7.chunk.css"
+  },
+  {
+    "revision": "3132dcd0757829015cd21d6b9c8fb135",
+    "url": "/ClickyGame/index.html"
+  }
+];
